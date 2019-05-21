063a2417f33614b86edd331a174c5e321db0538d
Chinathip pondumpai <cc.chinathip@gmail.com>
2019-05-21 15:43:11 +0700

    lineruby2

--- app.rb
+++ app.rb
@@ -14,33 +14,33 @@ get '/' do
   "Hello World99"
 end
 
-    post '/callback' do
-    body = request.body.read
+  #   post '/callback' do
+  #   body = request.body.read
 
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
+  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #   unless client.validate_signature(body, signature)
+  #     error 400 do 'Bad Request' end
+  #   end
 
-    events = client.parse_events_from(body)
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
+  #   events = client.parse_events_from(body)
+  #   events.each do |event|
+  #     case event
+  #     when Line::Bot::Event::Message
+  #       case event.type
+  #       when Line::Bot::Event::MessageType::Text
 
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+  #         message = {
+  #           type: 'text',
+  #           text: event.message['text']
+  #         }
 
-          client.reply_message(event['replyToken'], message)
-        end
-      end
+  #         client.reply_message(event['replyToken'], message)
+  #       end
+  #     end
 
-    'OK'
-    end
-  end
+  #   'OK'
+  #   end
+  # end
 
   
   
