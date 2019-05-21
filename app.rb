6bc5d66165e10f2c0a5ab2b654d29785d6dc43ad
Chinathip pondumpai <cc.chinathip@gmail.com>
2019-05-21 16:08:17 +0700

    test4

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require 'json'
 require 'line/bot'
 require 'dotenv/load'
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
+# def client
+#   @client ||= Line::Bot::Client.new { |config|
+#     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+#     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+#   }
+# end
 
 get '/' do
   erb :index
